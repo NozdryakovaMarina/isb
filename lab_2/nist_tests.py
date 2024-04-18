71f9292b2e6ff31a8ba711864fb4a120309aa7b2
Marina <nozdryakovamarina94@gmail.com>
2024-04-18 18:49:56 +0400

    test3

--- lab_2/nist_tests.py
+++ lab_2/nist_tests.py
@@ -1,9 +1,11 @@
 import json
 import math
 
+import mpmath
+
 from typing import Dict
 
-from const import path
+from const import path, PI
 
 
 def get_json(name: str) -> Dict[str, str]:
@@ -37,11 +39,11 @@ def frequency_bitwise_test(name: str) -> float:
             P-value is the probability that the
             generator produces values comparable to the reference
     """
-    s = abs(sum(1 if i == "1" else -1 for i in name))/math.sqrt(len(name))
-    return math.erfc(s/math.sqrt(2))
+    s = abs(sum(1 if i == "1" else -1 for i in name)) / math.sqrt(len(name))
+    return math.erfc(s / math.sqrt(2))
 
 
-def identical_consecutive_bits_test(name: str):
+def identical_consecutive_bits_test(name: str) -> float:
     """
     The function searches for all sequences of identical bits,
     and then analyzes how much the number and sizes of these
@@ -54,18 +56,66 @@ def identical_consecutive_bits_test(name: str):
              produces values comparable to the reference
     """
     n = len(name)
-    prop_units = name.count("1")/n
+    prop_units = name.count("1") / n
     if abs(prop_units - 0.5) >= 2 / math.sqrt(n):
         return 0
     v = sum(0 if name[i] == name[i + 1] else 1 for i in range(n - 1))
     return math.erfc(abs(v - 2 * n * prop_units * (1 - prop_units)) /
-                    (2 * math.sqrt(2 * n * prop_units * (1 - prop_units))))
+                     (2 * math.sqrt(2 * n * prop_units * (1 - prop_units))))
+
+
+def max_sequence_units(name: str) -> int:
+    """
+    The function searches for the maximum length of a
+    sequence of consecutive units
+
+    Args:
+             name: path .json file
+    Returns:
+             maximum: longest sequence units
+    """
+    maximum = 0
+    count = 0
+    for i in name:
+        if i == "1":
+            count += 1
+            maximum = max(maximum, count)
+        else:
+            maximum = max(maximum, count)
+            count = 0
+    return maximum
+
+
+def longest_sequence_units_test(name: str) -> float:
+    """
+    In this test, the longest row of units inside a block
+    with a length of m bits is determined
+
+    Args:
+             name: path .json file
+    Returns:
+             float: P-value
+    """
+    n = len(name)
+    m = 8
+    count_max = []
+    for i in range(0, n, m):
+        block_len = count_max.append(max_sequence_units(name[i: i + 8]))
+    v1 = count_max.count(0) + count_max.count(1)
+    v2 = count_max.count(2)
+    v3 = count_max.count(3)
+    v4 = len(count_max) - v1 - v2 - v3
+    v = [v1, v2, v3, v4]
+
+    hi = sum((v[i] - 16 * PI[i])** 2 / (16 * PI[i]) for i in range(4))
+    return mpmath.gammainc(1.5, hi / 2)
 
 
 def main() -> None:
-    name = get_json(path)["java"]
+    name = get_json(path)["cpp"]
     frequency_bitwise_test(name)
     identical_consecutive_bits_test(name)
+    longest_sequence_units_test(name)
 
 
 if __name__ == "__main__":
